79d1356c3e2b4b4e90fbc30b8fb5ff22ae286847
nikita <nikita-ko85@mail.ru>
2025-03-21 11:59:28 +0500

    Добавил stories для компонентов страниц

---
+++ src/pages/AboutPage/ui/AboutPage.stories.ts
@@ -0,0 +1,26 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {ThemeDecorator} from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "@/app/providers/ThemProvider";
+import {AboutPage} from "@/pages/AboutPage";
+
+const meta = {
+    title: 'pages/AboutPage',
+    component: AboutPage,
+    parameters: {
+        layout: 'centered'
+    },
+    tags: ['autodocs'],
+    argTypes: {
+    },
+    args: {}
+} satisfies Meta<typeof AboutPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+};
+
+export const Dark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)]
+};
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.ts
@@ -0,0 +1,26 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {ThemeDecorator} from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "@/app/providers/ThemProvider";
+import {NotFoundPage} from "@/pages/NotFoundPage";
+
+const meta = {
+    title: 'pages/NotFoundPage',
+    component: NotFoundPage,
+    parameters: {
+        layout: 'centered'
+    },
+    tags: ['autodocs'],
+    argTypes: {
+    },
+    args: {}
+} satisfies Meta<typeof NotFoundPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+};
+
+export const Dark: Story = {
+    decorators: [ThemeDecorator(Theme.DARK)]
+};
